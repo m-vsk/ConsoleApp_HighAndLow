367f0799ac7b80f23efaca4630d392c8ff87a859
vsk <m-vsk@yandex.ru>
2018-01-09 01:34:55 +0300

    Highest and Lowest

--- HighAndLow/Program.cs
+++ HighAndLow/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,18 @@ namespace HighAndLow
 {
     class Program
     {
+        private static string HighAndLow(string value)
+        {
+            String[] substrings = value.Split();
+            int[] nums = substrings.Select(int.Parse).ToArray();
+            return nums.Max() + " " + nums.Min();
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+            Console.ReadKey();
         }
+
     }
 }
